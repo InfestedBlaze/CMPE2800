c72a268a2ed588416ecf34ec0120f569b8b4349d
Nicholas Wasylyshyn <nicwasylyshyn@gmail.com>
2018-01-16 08:12:44 -0700

    Retry
    Needed gitIgnore

---
+++ Labs/ReviewLab/ReviewLab/Block.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ReviewLab
+{
+    //Enums to determine state
+    enum Life { Alive, Dying}  //Whether the block is dying or not
+    enum Fall { Still, Falling} //Whether the block is falling (Downward displacement)
+
+    //An item in the grid
+    abstract class Block
+    {
+        private Color colour;
+        public Color Colour { get { return colour; } protected set { colour = value; } }
+        public Life life = Life.Alive;
+        public byte AnimationState = 0;
+    }
+
+    //A block that is able to fall to the next area
+    class FreeBlock : Block
+    {
+        public Fall fall = Fall.Still;
+        public FreeBlock ()
+        {
+            Colour = Color.Red;
+        }
+    }
+
+    //A block that does not move around the grid
+    class SolidBlock : Block
+    {
+        public SolidBlock ()
+        {
+            Colour = Color.Orange;
+        }
+    }
+
+    //A nonmoving block that will save a space for a falling block
+    //Size is irrelevant. Only made to occupy grid
+    class RetainerBlock : Block
+    {
+        public RetainerBlock ()
+        {
+            Colour = Color.Transparent;
+        }
+    }
+}
---
+++ Labs/ReviewLab/ReviewLab/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+
+namespace ReviewLab
+{
+    public partial class Form1 : Form
+    {
+
+        //Window and block constansts
+        private const int WindowXSize = 1000;
+        private const int WindowYSize = 1000;
+        private const int BlockSize   = 100; //Assumed to be greater than 10
+
+        //Canvas to display our grid
+        CDrawer canvas = new CDrawer(WindowXSize, WindowYSize, false, true);
+        //Grid
+        Grid grid = new Grid(WindowXSize/BlockSize, WindowYSize/BlockSize);
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            canvas.MouseLeftClickScaled += Canvas_MouseLeftClickScaled;
+            canvas.MouseRightClickScaled += Canvas_MouseRightClickScaled;
+        }
+
+        private void Canvas_MouseLeftClickScaled(Point pos, CDrawer dr)
+        {
+            //Scale to our grid
+            pos.X /= BlockSize;
+            pos.Y /= BlockSize;
+
+            //Add a falling block
+            grid.AddBlock(pos, false);
+            //Something changed, enable the timer
+            timer.Enabled = true;
+        }
+
+        private void Canvas_MouseRightClickScaled(Point pos, CDrawer dr)
+        {
+            //Scale to our grid
+            pos.X /= BlockSize;
+            pos.Y /= BlockSize;
+
+            if (Control.ModifierKeys == Keys.Shift)
+            {
+                //Add a solid block
+                grid.AddBlock(pos, true);
+            }
+            else
+            {
+                grid.RemoveBlock(pos);
+            }
+            //Something changed, enable the timer
+            timer.Enabled = true;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //Do a tick of grid action. It will determine whether we continue
+            timer.Enabled = grid.Tick();
+            //Show the change
+            grid.Render(canvas, BlockSize);
+            //On the console as well
+            grid.Write();
+        }
+    }
+}
