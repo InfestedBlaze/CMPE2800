e7aa954bf7290b39c24facbae54a31097c0ed51c
Nicholas B Wasylyshyn <nwasylyshyn1@nait.ca>
2018-02-05 11:43:55 -0700

    ICA 4
    Classes added

---
+++ ICA/ICA4_NicW/ICA4_NicW/Shapes.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Threading.Tasks;
+
+namespace ICA4_NicW
+{
+    abstract class BaseShape
+    {
+        //Members
+        protected float rotation;
+        protected float rotIncrement;
+        protected float XSpeed;
+        protected float YSpeed;
+        public const int TILESIZE = 50;
+
+        //Static members
+        static Random randNum;
+
+        //Fields
+        public bool IsMarkedForDeath { get; set; }
+        public PointF Position { get; set; }
+
+        //Methods
+        abstract protected GraphicsPath GetPath();
+
+        public void Render()
+        {
+
+        }
+
+        protected GraphicsPath ShapeBase(int points, bool circle)
+        {
+            return new GraphicsPath();
+        }
+
+        public void Tick()
+        {
+
+        }
+
+        public BaseShape(PointF inPoint)
+        {
+            Position = inPoint;
+
+            rotation = 0;
+            rotIncrement = randNum.Next(-3, 3);
+
+            XSpeed = (float)(randNum.NextDouble() * 5 - 2.5);
+            YSpeed = (float)(randNum.NextDouble() * 5 - 2.5);
+        }
+
+        static BaseShape()
+        {
+            randNum = new Random();
+        }
+    }
+
+    class Triangle : BaseShape
+    {
+        static readonly GraphicsPath model;
+
+        protected override GraphicsPath GetPath()
+        {
+            //Make the transform
+            Matrix mat = new Matrix();
+            mat.Rotate(this.rotation);
+            mat.Translate(this.XSpeed, this.YSpeed, MatrixOrder.Append);
+            //Clone the model and apply the transform
+            GraphicsPath temp = (model.Clone() as GraphicsPath);
+            temp.Transform(mat);
+            //Return the transformed model
+            return temp;
+        }
+
+        public Triangle(PointF inPoint) : base (inPoint)
+        {}
+
+        static Triangle()
+        {
+            //Make the triangle model
+            model = ShapeBase(3, true);
+        }
+    }
+}
