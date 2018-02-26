f387fac13e84b680c6798d96379c4a34e5a9cdeb
Nicholas B Wasylyshyn <nwasylyshyn1@nait.ca>
2018-02-26 14:51:18 -0700

    More done for our threading

--- Labs/Asteroids/InputControls/InputControls.cs
+++ Labs/Asteroids/InputControls/InputControls.cs
@@ -5,43 +5,95 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace InputControls
 {
     public class InputControls
     {
+        //Determines if the XBOX controller plugged in
         private bool controllerAttached = false;
 
-        static private Dictionary<Keys, bool> keys;
-        static public Dictionary<Keys, bool> Inputs { get { return keys; } }
+        //Our keys to determine what is pressed
+        static private Dictionary<System.Windows.Forms.Keys, bool> keys;
+        static public Dictionary<System.Windows.Forms.Keys, bool> Inputs { get { return keys; } }
         
+        //Constructors
+        public InputControls()
+        {
+            Thread xboxPolling = new Thread(XboxPollingThread);
+            xboxPolling.IsBackground = true;
+            xboxPolling.Start();
+        }
         static InputControls()
         {
             //Fills the dictionary with all of the key values
-            foreach(Keys allKeys in Enum.GetValues(typeof(Keys)))
+            foreach(System.Windows.Forms.Keys allKeys in Enum.GetValues(typeof(System.Windows.Forms.Keys)))
             {
                 keys[allKeys] = false;
             }
         }
 
+        //Functions that change our input states
         /// <summary>
-        /// This will change the key inputted to true
+        /// This will change the key inputted to true, doesn't work if XBOX is plugged in
         /// </summary>
         /// <param name="inKey">The key that is pressed down</param>
-        public void KeyDown(Keys inKey)
+        public void KeyDown(System.Windows.Forms.Keys inKey)
         {
-            if(!controllerAttached) //No keyboard if the XBOX controller is plugged in
-                keys[inKey] = true;
+            lock (keys)
+            {
+                if (!controllerAttached) //No keyboard if the XBOX controller is plugged in
+                    keys[inKey] = true;
+            }
         }
 
         /// <summary>
-        /// This will change the key inputted to false
+        /// This will change the key inputted to false, doesn't work if XBOX is plugged in
         /// </summary>
         /// <param name="inKey">The key that is released</param>
-        public void KeyUp(Keys inKey)
+        public void KeyUp(System.Windows.Forms.Keys inKey)
         {
-            if(!controllerAttached) //No keyboard if the XBOX controller is plugged in
-                keys[inKey] = false;
+            lock (keys)
+            {
+                if (!controllerAttached) //No keyboard if the XBOX controller is plugged in
+                    keys[inKey] = false;
+            }
+        }
+
+        private void XboxPollingThread()
+        {
+            while (true)
+            {
+                //Get the state of player one
+                GamePadState gps = GamePad.GetState(PlayerIndex.One);
+
+                //Check if the controller is attached
+                controllerAttached = gps.IsConnected;
+                //If the controller is attached then we only look at it for inputs
+                if (controllerAttached)
+                {
+                    //Lock out our key inputs
+                    lock (keys)
+                    {
+                        //Left thumbstick is mapped to WASD
+                        keys[System.Windows.Forms.Keys.W] = gps.ThumbSticks.Left.Y > 0;
+                        keys[System.Windows.Forms.Keys.S] = gps.ThumbSticks.Left.Y < 0;
+                        keys[System.Windows.Forms.Keys.A] = gps.ThumbSticks.Left.X < 0;
+                        keys[System.Windows.Forms.Keys.D] = gps.ThumbSticks.Left.X > 0;
+
+                        //The A button is mapped to Spacebar
+                        keys[System.Windows.Forms.Keys.Space] = gps.IsButtonDown(Buttons.A);
+
+                        //The start button is mapped to P
+                        keys[System.Windows.Forms.Keys.P] = gps.IsButtonDown(Buttons.Start);
+                    }
+                }
+
+                //Wait for more inputs
+                Thread.Sleep(25);
+            }
         }
     }
 }
