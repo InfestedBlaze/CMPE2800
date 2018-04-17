0466ab40a5fd1a1c09e3834cbee6d8f1b49c184c
Nicholas B Wasylyshyn <nwasylyshyn1@nait.ca>
2018-04-17 09:24:15 -0600

    Line Drawer Server
    Cool stuff

---
+++ Labs/LineDrawerServer/LineDrawerServer/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace LineDrawerServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.UI_toolStripStatusLabel_TotalBytes = new System.Windows.Forms.ToolStripStatusLabel();
+            this.UI_toolStripStatusLabel_TotalFrames = new System.Windows.Forms.ToolStripStatusLabel();
+            this.UI_dataGridView_Clients = new System.Windows.Forms.DataGridView();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UI_dataGridView_Clients)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.UI_toolStripStatusLabel_TotalBytes,
+            this.UI_toolStripStatusLabel_TotalFrames});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 390);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(316, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // UI_toolStripStatusLabel_TotalBytes
+            // 
+            this.UI_toolStripStatusLabel_TotalBytes.Name = "UI_toolStripStatusLabel_TotalBytes";
+            this.UI_toolStripStatusLabel_TotalBytes.Size = new System.Drawing.Size(75, 17);
+            this.UI_toolStripStatusLabel_TotalBytes.Text = "Total Bytes: -";
+            // 
+            // UI_toolStripStatusLabel_TotalFrames
+            // 
+            this.UI_toolStripStatusLabel_TotalFrames.Name = "UI_toolStripStatusLabel_TotalFrames";
+            this.UI_toolStripStatusLabel_TotalFrames.Size = new System.Drawing.Size(85, 17);
+            this.UI_toolStripStatusLabel_TotalFrames.Text = "Total Frames: -";
+            // 
+            // UI_dataGridView_Clients
+            // 
+            this.UI_dataGridView_Clients.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_dataGridView_Clients.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.UI_dataGridView_Clients.Location = new System.Drawing.Point(13, 12);
+            this.UI_dataGridView_Clients.Name = "UI_dataGridView_Clients";
+            this.UI_dataGridView_Clients.Size = new System.Drawing.Size(291, 375);
+            this.UI_dataGridView_Clients.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 412);
+            this.Controls.Add(this.UI_dataGridView_Clients);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Line Drawer Server";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UI_dataGridView_Clients)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel UI_toolStripStatusLabel_TotalBytes;
+        private System.Windows.Forms.ToolStripStatusLabel UI_toolStripStatusLabel_TotalFrames;
+        private System.Windows.Forms.DataGridView UI_dataGridView_Clients;
+    }
+}
+
---
+++ Labs/LineDrawerServer/LineDrawerServer/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using mdtypes;
+
+namespace LineDrawerServer
+{
+    public partial class Form1 : Form
+    {
+        private delegate void delVoidSocket(Socket x);
+        private delegate void delVoidVoid();
+
+        private Socket listenerSocket = null;
+        private List<Connection> clientList = new List<Connection>();
+
+        private Queue<LineSegment> lines = new Queue<LineSegment>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Thread thread = new Thread(SendLinesThread);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            try
+            {
+                listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                listenerSocket.Bind(new System.Net.IPEndPoint(System.Net.IPAddress.Any, 1666));
+
+                StartListen();
+            }
+            catch
+            {
+                cbError("Form1 Shown Error");
+            }
+        }
+
+        private void StartListen()
+        {
+            try
+            {
+                //Listen for clients
+                listenerSocket.Listen(5);
+                //Begin accepting them
+                listenerSocket.BeginAccept(EndAccept, null);
+            }
+            catch
+            {
+                cbError("Listen Error");
+            }
+        }
+
+        private void EndAccept(IAsyncResult ar)
+        {
+            try
+            {
+                //Make a temporary socket that is the connection
+                Socket connect = listenerSocket.EndAccept(ar);
+                //Go into our handling function
+                Invoke(new delVoidSocket(HandleAccept), connect);
+            }
+            catch
+            {
+                cbError("Accept Error");
+            }
+
+            //Listen for more clients
+            StartListen();
+        }
+
+        private void HandleAccept(Socket connectedSocket)
+        {
+            //Make a temporary connection and add it to the client list
+            Connection temp = new Connection(connectedSocket, cbDisconnect, cbError, cbLine);
+            lock (clientList)
+                clientList.Add(temp);
+        }
+
+        private void cbDisconnect(Connection connection, string message)
+        {
+            //Remove the connection from our client list
+            lock (clientList)
+                clientList.Remove(connection);
+        }
+
+        private void cbError(string message)
+        {
+            //Set the error message to the title bar
+            Invoke(new Action(() => this.Text = message));
+        }
+
+        private void cbLine(LineSegment ls)
+        {
+            //Send each line segment to our queue
+            lock (lines)
+                lines.Enqueue(ls);
+        }
+        
+        private void SendLinesThread()
+        {
+            LineSegment sendLine;
+            while (true)
+            {
+                if (lines.Count == 0)
+                    continue;
+                
+                lock (lines)
+                    sendLine = lines.Dequeue();
+
+                lock (clientList)
+                    clientList.ForEach(client => client.SendData(sendLine));
+
+                Invoke(new delVoidVoid(bindData));
+            }
+        }
+
+        private void bindData()
+        {
+            BindingSource bs = new BindingSource();
+            lock(clientList)
+                bs.DataSource = from client in clientList
+                                select new
+                                {
+                                    Address = client.address,
+                                    FramesReceieved = client.framesRx,
+                                    DestackAverage = client.framesRx / client.receiveEvents,
+                                    Fragments = client.fragments
+                                };
+            UI_dataGridView_Clients.DataSource = bs;
+
+            long Frames, Bytes;
+            lock (clientList)
+            {
+                Frames = clientList.Sum(client => client.framesRx);
+                Bytes = clientList.Sum(client => client.bytesRx);
+            }
+            UI_toolStripStatusLabel_TotalFrames.Text = $"Total Frames: {Frames}";
+            UI_toolStripStatusLabel_TotalBytes.Text = $"Total Bytes: {Bytes}";
+        }
+    }
+}
